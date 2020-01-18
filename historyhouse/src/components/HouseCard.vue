517659a87270a61eb65f1741db277b80b217218d
Vuejs <vuejs@zMacPro-8.local>
2020-01-18 14:24:46 +0100

    create housecard props

---
+++ historyhouse/src/components/HouseCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h3>Your house information</h3>
+        <p><strong>Name:</strong> {{houseInformation.name}}</p>
+        <p v-if="houseInformation.school"><strong>School:</strong> {{houseInformation.school}}</p>
+        <strong>Colors:</strong> <div v-for="(color, index) of houseInformation.colors" :key="`color-${index}`">{{color}}</div>
+        <strong>Values:</strong> <div v-for="(value, index) of houseInformation.values" :key="`value-${index}`">{{value}}</div>
+        <p><strong>Founder:</strong> {{houseInformation.founder}}</p>
+        <p><strong>Head of the House: </strong>{{houseInformation.headOfHouse}}</p>
+        <p><strong>Ghost:</strong> {{houseInformation.houseGhost}}</p>
+        <p><strong>Mascot:</strong> {{houseInformation.mascot}}</p>
+        <strong>Famous Members</strong><div v-for="(member, index) of houseInformation.members" :key="`member-${index}`">{{member.name}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            houseInformation: {
+                type: Object,
+                default: () => ({}),
+            }
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
