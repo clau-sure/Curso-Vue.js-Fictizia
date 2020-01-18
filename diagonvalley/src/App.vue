65be598e751b0a4432833a740c5daddac0cb853f
Vuejs <vuejs@zMacPro-8.local>
2020-01-18 12:19:23 +0100

    diagonalley computed

--- diagonvalley/src/App.vue
+++ diagonvalley/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DiagonValley></DiagonValley>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DiagonValley from './components/DiagonValley.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    DiagonValley
   }
 }
 </script>
---
+++ diagonvalley/src/components/DiagonValley.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>  
+        <h2>Flourish and Blotts</h2>
+        <div>
+            <h3>Products</h3>
+            
+            <div v-for="product in productList" :key="product.id">
+                Title: {{product.title}}
+                Quantity: {{product.quantity}}
+                Price: {{product.price}}
+            </div>
+        </div>
+        <div>
+            <h3>Your carrier</h3>
+            <p>Total: {{totalPrice}} galeons</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                productList: [
+                    {
+                        title: "The Tales of Beedle the Bard",
+                        quantity: 2,
+                        price: 15,
+                        id: 1
+                    },
+                    {
+                        title: "A History of Magic by Bathilda Bagshot",
+                        quantity: 1,
+                        price: 45,
+                        id: 2
+                    },
+                    {
+                        title: "The Life and Lies of Albus Dumbledore",
+                        quantity: 5,
+                        price: 22,
+                        id: 3
+                    },
+                ]
+            }
+        },
+        computed: {
+            totalPrice({productList}) {
+                var totalPrice=""
+                productList.forEach(product => {
+                    totalPrice += product.quantity * product.price
+                });
+                return totalPrice;
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
