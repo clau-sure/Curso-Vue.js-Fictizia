bf19cbe93b87fd578611484d704cea189206d87a
Vuejs <vuejs@zMacPro-8.local>
2020-01-25 11:07:56 +0100

    hook created

--- historyhouse/src/components/HistoryHouse.vue
+++ historyhouse/src/components/HistoryHouse.vue
@@ -2,7 +2,7 @@
   <div>
     <h2>Select a Hogwarts house</h2>
     <select v-model="selected">
-      <option value="5a05dc8cd45bd0a11bd5e071">Slytherin</option>
+      <option value="5a05dc8cd45bd0a11bd5e071" selected>Slytherin</option>
       <option value="5a05e2b252f721a3cf2ea33f">Griffindor</option>
       <option value="5a05da69d45bd0a11bd5e06f">Ravenclaw</option>
       <option value="5a05dc58d45bd0a11bd5e070">Hufflepuff</option>
@@ -21,14 +21,24 @@ export default {
     },
     data() {
         return {
-            selected: "",
+            selected: "5a05dc8cd45bd0a11bd5e071",
             key: "$2a$10$RqwyBB1PLMaKYyCqnF5VKu5w9wJIpOqAo0M1hxwjV2APITXwg5K/G",
             houseId: '',
             house: {}
         }
     },
+    created() {
+        this.getHouses(this.selected)
+    },
+
     watch: {
         selected(houseId){
+            this.getHouses(houseId)
+        }
+    },
+
+    methods: {
+        getHouses(houseId) {
             fetch(`https://www.potterapi.com/v1/houses/${houseId}?key=${this.key}`)
             .then(function(response){
                 return response.json();
